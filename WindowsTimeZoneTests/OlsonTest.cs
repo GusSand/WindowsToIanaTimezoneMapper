f201469fd5c963eb184ed9465e1bc522be8e9f6b
Gustavo Sandoval <gustavo.sandoval@skype.net>
2014-12-16 17:48:08 -0500

    First version with unit tests

---
+++ WindowsTimeZoneTests/OlsonTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsTimeZoneToTzidMapper;
+
+namespace WindowsTimeZoneTests
+{
+
+    [TestClass]
+    public class OlsonTest
+    {
+        private OlsonMapper _mapper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            string path = @"..\..\..\WindowsTimeZoneToTzidMapper\currenttimezones.xml";
+            _mapper = new OlsonMapper(path);
+        }
+
+        // mapZone other="Eastern Standard Time" territory="001" type="America/New_York"/>
+        [TestMethod]
+        public void TestMethodEasternTime()
+        {
+            const string timezoneId = "Eastern Standard Time";
+            const string olsonString = "America/New_York";
+            var result = _mapper.Find(timezoneId);
+            Assert.AreEqual(olsonString, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethodPacificTime()
+        {
+            // ("Pacific Standard Time", "America/Los_Angeles")]
+            const string timezoneId = "Pacific Standard Time";
+            const string olsonString = "America/Los_Angeles";
+            var result = _mapper.Find(timezoneId);
+            Assert.AreEqual(olsonString, result);
+        }
+
+        [TestMethod]
+        public void TestMethodEurope()
+        {
+            // <mapZone other="W. Europe Standard Time" territory="001" type="Europe/Berlin"/>
+            const string timezoneId = "W. Europe Standard Time";
+            const string olsonString = "Europe/Berlin";
+            var result = _mapper.Find(timezoneId);
+            Assert.AreEqual(olsonString, result);
+
+        }
+
+        [TestMethod]
+        public void TestMethodUnknown()
+        {
+            // unknown
+            const string timezoneId = "Unknown Id";
+            var result = _mapper.Find(timezoneId);
+            Assert.AreEqual(timezoneId, result);
+
+
+        }
+
+
+
+    }
+}
---
+++ WindowsTimeZoneToTzidMapper/OlsonMapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace WindowsTimeZoneToTzidMapper
+{
+    public class OlsonMapper
+    {
+        private readonly Dictionary<string, string> _mapping;
+        private const string _xmlPath = @"..\..\WindowsTimeZoneToTzidMapper\currenttimezones.xml";
+
+
+        public OlsonMapper(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                path = _xmlPath;
+
+            var xml = XElement.Load(path);
+            var values = from m in xml.XPathSelectElements("*/mapTimezones/mapZone")
+                where m.Attribute("territory").Value == "001"
+                select new
+                {
+                    TimeZoneId = m.Attribute("other").Value,
+                    OlsonId = m.Attribute("type").Value
+                };
+
+            _mapping = values.ToDictionary(v => v.TimeZoneId, v => v.OlsonId);
+
+
+        }
+
+        public static void Main()
+        {
+            
+        }
+
+        public string Find(string timeZoneId)
+        {
+            if (!_mapping.ContainsKey(timeZoneId))
+                return timeZoneId;
+
+            return _mapping[timeZoneId];
+        }
+    }
+}
